d73874447db6649f3599196751ce8235e157ce8f
Michael Foos <michaelpfoos@gmail.com>
2024-06-24 12:09:11 -0400

    updating code to after finishing chapter

--- vite-starter/src/App.jsx
+++ vite-starter/src/App.jsx
@@ -1,16 +1,21 @@
 import React, {useState} from 'react';
 import './app.css';
+import { kebabCaseToTitleCase } from './helper';
 
 function App() {
-  const [buttonColor, setButtonColor] = useState("red");
-  const nextColor = buttonColor === "red" ? "blue" : "red";
+  const [buttonColor, setButtonColor] = useState("medium-violet-red");
+  const nextColor = buttonColor === "medium-violet-red" ? "midnight-blue" : "medium-violet-red";
   const [disabled, setDisabled] = useState(false);
+
+  const toggleCheckBox = (e) => {
+    setDisabled(e.target.checked);      
+  };
 
   return (
     <div>
-      <button className={buttonColor} onClick={() => {setButtonColor(nextColor)}} >Change to {nextColor}</button>
+      <button className={disabled ? "gray" : buttonColor} disabled={disabled} onClick={() => {setButtonColor(nextColor)}} >Change to {nextColor}</button>
       <br />
-      <input type="checkbox" id="disable-button-checkbox" checked={disabled} />
+      <input type="checkbox" id="disable-button-checkbox" onClick={toggleCheckBox} />
       <label htmlFor="disable-button-checkbox">Disable button</label>
     </div>
   );
--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
+import { kebabCaseToTitleCase } from "./helper";
 
 test("button click flow", () => {
   //render the app  
@@ -9,7 +10,7 @@ test("button click flow", () => {
   const buttonElement = screen.getByRole("button", {name: /blue/i });
 
   // check the initial color
-  expect(buttonElement).toHaveClass("red");
+  expect(buttonElement).toHaveClass("medium-violet-red");
 
   // click the button
   fireEvent.click(buttonElement);
@@ -18,9 +19,10 @@ test("button click flow", () => {
   expect(buttonElement).toHaveTextContent(/red/i);
 
   // check button color
-  expect(buttonElement).toHaveClass("blue");
+  expect(buttonElement).toHaveClass("midnight-blue");
 });
 
+//code quiz add checkbox functionality
 test("checkbox flow", () => {
   //render the app  
   render(<App />);
@@ -37,5 +39,55 @@ test("checkbox flow", () => {
 
   //when checkbox is checked button should be disabled
   fireEvent.click(checkboxElement);
+  expect(buttonElement).toBeDisabled();
+
+  //when checkbox is unchecked button should be enabled
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toBeEnabled();
   
+});
+
+test("Code Quiz! Button Gray when disabled", () => {
+  render(<App />);
+  const buttonElement = screen.getByRole("button", { name: /blue/i });
+  const checkboxElement = screen.getByRole("checkbox", { name: /disable button/i });
+
+  //check initial conditions
+  expect(buttonElement).toHaveClass("medium-violet-red");
+  expect(buttonElement).toBeEnabled();
+  expect(checkboxElement).not.toBeChecked();
+
+  //when checkbox is checked button should be disabled
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toHaveClass("gray");
+  expect(buttonElement).toBeDisabled();
+
+  //when checkbox is unchecked button should be enabled
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toHaveClass("medium-violet-red");
+  expect(buttonElement).toBeEnabled();
+
+  //change button to blue
+  fireEvent.click(buttonElement);
+  expect(buttonElement).toHaveClass("midnight-blue");
+
+  //when checkbox is checked button should be disabled
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toHaveClass("gray");
+
+  //enable button to make it blue again
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toHaveClass("midnight-blue");
+});
+
+describe("kebabCaseToTitleCase", () => {
+  test("works for no hyphens", () => {
+    expect(kebabCaseToTitleCase("red")).toBe("Red");
+  });
+  test("works for one hyphen", () => {  
+    expect(kebabCaseToTitleCase("midnight-blue")).toBe("Midnight Blue");
+  });
+  test("works for multiple hyphens", () => {  
+    expect(kebabCaseToTitleCase("medium-violet-red")).toBe("Medium Violet Red");
+  });
 });
